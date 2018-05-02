eebcf76672ff9b32f726a7070ee74446e1eb6704
ruoru <cruoru@gmail.com>
2018-05-03 00:51:44 +0800

    add cargo in readme

--- src/01/01.rs
+++ src/01/01.rs
@@ -1,3 +1,13 @@
 fn main() {
-  println!("Hello, world!");
+  use std::io;
+
+  println!("What is your name?");
+
+  let mut input = String::new();
+
+  io::stdin().read_line(&mut input).unwrap();
+
+  let message = "Hello,".to_string()+input.trim()+",nice to meet you!";
+
+  println!("{}",message);
 }
